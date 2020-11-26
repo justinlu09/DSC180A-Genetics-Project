d6732e584940993064fde37fd006dd15482de29d
justinlu09 <justinlu09@gmail.com>
2020-11-25 18:31:08 -0800

    filtering gene matrix more

--- src/process.py
+++ src/process.py
@@ -9,14 +9,11 @@ logging.basicConfig(filename="log.txt", filemode='a',
 
 def quality_check(data_dir, fastqc_path, fq_output_bc):
     data = sorted(os.listdir(data_dir))
-    count = 0
-    lst = []
-    
+#     count = 0
     logging.info("Starting FastQC...")
     for j in range(0, len(data), 2):
         if ((data[j][:12] + '_fastqc.html') in os.listdir(fq_output_bc)) and ((data[j+1] [:12] + '_fastqc.html') in os.listdir(fq_output_bc)):
             #count += 1
-            #break
             continue
         
         #pair of files (..._1.fastq.gz, ..._2.fastq.gz)
@@ -24,11 +21,12 @@ def quality_check(data_dir, fastqc_path, fq_output_bc):
         second_file = os.path.join(data_dir, data[j+1])
         
         logging.info("Running FastQC on " + str(data[j]) + " and " + str(data[j+1]))
+        
         #running fastqc on the pair of files before cutadapt
         os.system(fastqc_path + ' ' + first_file + ' ' + second_file + ' --outdir ' + fq_output_bc)
         
 #         count += 1
-#         if count == 30:
+#         if count == 16:
 #             break
     return
     
@@ -105,7 +103,9 @@ def alignment(data_dir, kallisto_path, kallisto_idx, kallisto_output):
             #count += 1 
             first_file = os.path.join(data_dir, data[i])
             second_file = os.path.join(data_dir, data[i+1])
+            
             logging.info("Running pair-end alignment with Kallisto on " + str(data[i]) + " and " + str(data[i+1]))
+            
             os.system(kallisto_path + ' quant -i ' + kallisto_idx + ' -b 8 -o ' + os.path.join(kallisto_output, data[i][:10]) + ' ' + first_file + ' ' + second_file + ' -t 8') #+ ' >>' + os.path.join(kallisto_output, data[i][:10],'pseudo.bam'))
         else:
             break
